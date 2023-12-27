165764d0de15a2b283550a5c67e575b11e48bdd5
Evandro Santos <vandersant7@gmail.com>
2023-12-27 12:13:26 -0300

    commit inicial

---
+++ src/js/validacao.js
@@ -0,0 +1,25 @@
+const inputsVazios = document.querySelectorAll('.input');
+
+const formulario = document.querySelector('.formulario');
+
+
+inputsVazios.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value !== "") {
+            input.classList.add('formulario-preenchido')
+        }
+    })
+})
+
+
+formulario.addEventListener('submit', function(event) { 
+    inputsVazios.forEach(input => {
+        if (input.value === "") {
+            event.preventDefault();
+            input.classList.add('mudar-cor');
+            input.nextElementSibling.classList.add('message-error');
+        } else {
+            return true
+        }
+    })
+})
